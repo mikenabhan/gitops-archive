1a0616822eb2a4bd446a1f3f3635f20d7a6fe32a
Mike Nabhan <13139061+mikenabhan@users.noreply.github.com>
2021-08-24 16:34:34 -0600

    fixing inventory vars

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,21 +1,12 @@
 resource "local_file" "AnsibleInventory" {
-  content = templatefile("inventory.tmpl",
+  content = templatefile("./ansible/inventory.tmpl",
     {
-      master-dns      = proxmox_vm_qemu.k3s_master_pve[*].name,
-      master-ip       = proxmox_vm_qemu.k3s_master_pve[*].default_ipv4_address,
-      master-id       = proxmox_vm_qemu.k3s_master_pve[*].name,
-      master-dns-pve2 = proxmox_vm_qemu.k3s_master_pve2[*].name,
-      master-ip-pve2  = proxmox_vm_qemu.k3s_master_pve2[*].default_ipv4_address,
-      master-id-pve2  = proxmox_vm_qemu.k3s_master_pve2[*].name,
-      worker-dns      = proxmox_vm_qemu.k3s_worker_pve[*].name,
-      worker-ip       = proxmox_vm_qemu.k3s_worker_pve[*].default_ipv4_address,
-      worker-id       = proxmox_vm_qemu.k3s_worker_pve[*].name,
-      worker-dns-pve2 = proxmox_vm_qemu.k3s_worker_pve2[*].name,
-      worker-ip-pve2  = proxmox_vm_qemu.k3s_worker_pve2[*].default_ipv4_address,
-      worker-id-pve2  = proxmox_vm_qemu.k3s_worker_pve2[*].name,
-      etcd-dns        = proxmox_vm_qemu.k3s_etcd[*].name,
-      etcd-ip         = proxmox_vm_qemu.k3s_etcd[*].default_ipv4_address,
-      etcd-id         = proxmox_vm_qemu.k3s_etcd[*].name
+      master-dns      = proxmox_vm_qemu.gitops_master_pve[*].name,
+      master-ip       = proxmox_vm_qemu.gitops_master_pve[*].default_ipv4_address,
+      master-id       = proxmox_vm_qemu.gitops_master_pve[*].name,
+      master-dns-pve2 = proxmox_vm_qemu.gitops_master_pve2[*].name,
+      master-ip-pve2  = proxmox_vm_qemu.gitops_master_pve2[*].default_ipv4_address,
+      master-id-pve2  = proxmox_vm_qemu.gitops_master_pve2[*].name,
       network-domain  = var.personal_domain
     }
   )
@@ -27,7 +18,7 @@ output "playbook_command" {
 }
 
 output "get_kubeconfig" {
-  value = "k3sup install --skip-install --host ${proxmox_vm_qemu.k3s_master_pve[0].name}.${var.personal_domain}  --user ansible --local-path ~/.kube/config"
+  value     = "k3sup install --skip-install --host ${proxmox_vm_qemu.gitops_master_pve[0].name}.${var.personal_domain}  --user ansible --local-path ~/.kube/config"
   sensitive = false
 }
 
